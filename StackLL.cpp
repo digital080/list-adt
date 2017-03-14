5d72510f87a1c63bf46da0e993dbd97b397b641c
digital080 <saqensealy@hotmail.com>
2017-03-14 13:26:26 -0400

    Fixed StackLL.cpp

--- StackLL.cpp
+++ StackLL.cpp
@@ -19,76 +19,49 @@ class Stack::Node //self-referential Node class
 
 
 void Stack::push(int k){
+	
 	Node* newPtr = new Node{k};
-	Node* prevPtr = frontPtr;
 	char ch = '\0';
 	
-	if(num_elements == 0){
+	
 		newPtr -> link = frontPtr;
 		frontPtr = newPtr;
 		
 		//cout << frontPtr -> data << endl;
-		//ch = frontPtr -> data;
-		//cout << ch << endl; //Check to see if the elements are being linked
-		}else {
-	
-	
-	for(int x = 0; x < num_elements - 1; x++){//Stop at the element before the front of the stack
-			prevPtr = prevPtr -> link;
-			
-		}
+	//	ch = frontPtr -> data;
+	//	cout << ch << endl; //Check to see if the elements are being linked
 		
-		newPtr -> link = prevPtr -> link;
-		prevPtr -> link = newPtr;
 		
-
 		
-		}
 		num_elements++; //Incrementing num_elements crashes the program (Nothing to do with the for or if statement)
+	
 	}
 	
 int Stack::size(){
+	
 	return num_elements;
+	
 	}
 	
 		
-void Stack::pop(){
-	
+int Stack::pop(){
 	Node* delPtr = frontPtr;
 	int data2 = 0;
-	Node* prevPtr = frontPtr;
-	
-	if(num_elements > 0){
-			//delPtr -> link = frontPtr;
-			//frontPtr = delPtr;
-			//prevPtr = frontPtr;//prevPtr points to frontPtr
-		
-		
-	for(int x = 0; x < num_elements - 1; x++){//Make prevPtr point to 2nd last/front element
-			prevPtr = prevPtr -> link;
-		}
-		
-			delPtr = prevPtr -> link;//delPtr points to last/front of the stack
-			//prevPtr -> link = delPtr -> link;
-	
+
+		frontPtr = frontPtr -> link;
 		data2 = delPtr -> data;
 		delete delPtr;
 		num_elements--;
-		}else {
-			cout << "List is empty" << endl;
-			}
+		
+			return 0;
 	}
 
 
 	
 int Stack::top(){
-	Node* trackPtr = frontPtr;
 	
-	for(Node* currPtr = frontPtr; currPtr != nullptr; currPtr = currPtr -> link){
-		trackPtr = trackPtr -> link;
-		
-		}
-		
+	Node* trackPtr = frontPtr;
+
 		return trackPtr -> data;
 	}
 
@@ -103,8 +76,11 @@ void Stack::clear(){
 	}
 	
 Stack::~Stack()
-{
-    while(num_elements > 0)
+{/*
+    while(num_elements > 0){
       pop();
+       
+      num_elements--;
+  }*/
 }
 	
--- StackLL.h
+++ StackLL.h
@@ -24,7 +24,7 @@ public:
 
    void push(int);
 
-   void pop();
+   int pop();
 
    int top();
 
